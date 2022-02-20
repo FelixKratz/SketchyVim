30f53ca100f039189cc631e47fd07e4130f2c93b
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-02-20 13:54:35 +0100

    acquire lockfile

--- src/main.m
+++ src/main.m
@@ -6,9 +6,26 @@
 
 void* g_workspace;
 
+static void acquire_lockfile(void) {
+  int handle = open("/tmp/svim.lock", O_CREAT | O_WRONLY, 0600);
+  if (handle == -1) printf("Error: Could not create lock-file.\n"), exit(1);
+
+  struct flock lockfd = {
+    .l_start  = 0,
+    .l_len    = 0,
+    .l_pid    = getpid(),
+    .l_type   = F_WRLCK,
+    .l_whence = SEEK_SET
+  };
+
+  if (fcntl(handle, F_SETLK, &lockfd) == -1)
+    printf("Error: Could not acquire lock-file.\n"), exit(1);
+}
+
 int main (int argc, char *argv[]) {
   NSApplicationLoad();
 
+  acquire_lockfile();
   ax_begin(&g_ax);
   event_tap_begin(&g_event_tap);
   workspace_begin(&g_workspace);
